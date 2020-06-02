2c984386446c22b8afde96bb79c8ace665a42261
zwb <634700433@qq.com>
2020-06-02 09:40:02 +0800

    '卖家订单dao和service'

--- src/main/java/com/zwb/service/OrderService.java
+++ src/main/java/com/zwb/service/OrderService.java
@@ -15,7 +15,7 @@ public interface OrderService {
     OrderDTO create(OrderDTO orderDTO);
     //查询单个订单
     OrderDTO findOne(String orderId);
-    //查询订单列表
+    //查询订单列表，买家端，查询某个人
     Page<OrderDTO> findList(String buyerOpenid, Pageable pageable);
     //取消订单
     OrderDTO cancle(OrderDTO orderDTO);
@@ -23,4 +23,7 @@ public interface OrderService {
     OrderDTO finish(OrderDTO orderDTO);
     //支付订单
     OrderDTO paid(OrderDTO orderDTO);
+
+    //查询订单列表,卖家端，查询所有人
+    Page<OrderDTO> findList(Pageable pageable);
 }
--- src/test/java/com/zwb/service/impl/OrderServiceImplTest.java
+++ src/test/java/com/zwb/service/impl/OrderServiceImplTest.java
@@ -54,7 +54,7 @@ public class OrderServiceImplTest {
         orderDTO.setOrderDetailList(orderDetailList);
         OrderDTO result = orderService.create(orderDTO);
 
-        log.info("【创建订单】 result = {}",result);
+        log.info("【创建订单】 result = {}", result);
 
         Assert.assertNotNull(result);
     }
@@ -62,35 +62,43 @@ public class OrderServiceImplTest {
     @Test
     public void findOne() {
         OrderDTO result = orderService.findOne("123455");
-        log.info("【查询单个订单】 result = {}",result);
-        Assert.assertNotEquals(ORDERID,result.getOrderId());
+        log.info("【查询单个订单】 result = {}", result);
+        Assert.assertNotEquals(ORDERID, result.getOrderId());
     }
 
     @Test
     public void findList() {
-        PageRequest request = new PageRequest(0,2);
+        PageRequest request = new PageRequest(0, 2);
         Page<OrderDTO> orderDTOPage = orderService.findList(BUYER_OPENID, request);
-        Assert.assertNotEquals(0,orderDTOPage.getTotalElements());
+        Assert.assertNotEquals(0, orderDTOPage.getTotalElements());
     }
 
     @Test
     public void cancle() {
         OrderDTO orderDTO = orderService.findOne(ORDERID);
         OrderDTO result = orderService.cancle(orderDTO);
-        Assert.assertEquals(OrderStatusEnum.CANCEL.getCode(),result.getOrderStatus());
+        Assert.assertEquals(OrderStatusEnum.CANCEL.getCode(), result.getOrderStatus());
     }
 
     @Test
     public void finish() {
         OrderDTO orderDTO = orderService.findOne("123455");
         OrderDTO result = orderService.finish(orderDTO);
-        Assert.assertEquals(OrderStatusEnum.FINISHED.getCode(),result.getOrderStatus());
+        Assert.assertEquals(OrderStatusEnum.FINISHED.getCode(), result.getOrderStatus());
     }
 
     @Test
     public void paid() {
         OrderDTO orderDTO = orderService.findOne("123455");
         OrderDTO result = orderService.paid(orderDTO);
-        Assert.assertEquals(PayStatusEnum.SUCCESS.getCode(),result.getPayStatus());
+        Assert.assertEquals(PayStatusEnum.SUCCESS.getCode(), result.getPayStatus());
+    }
+
+    @Test
+    public void findListTest() {
+        PageRequest request = new PageRequest(0, 2);
+        Page<OrderDTO> orderDTOPage = orderService.findList(request);
+//        Assert.assertNotEquals(0, orderDTOPage.getTotalElements());
+        Assert.assertTrue("查询所有的订单列表", orderDTOPage.getTotalElements() > 0);
     }
 }
