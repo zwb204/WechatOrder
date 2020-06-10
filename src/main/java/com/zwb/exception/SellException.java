dbc7560bf1c004d1bb9a43d07ec53461dfc7d8d0
zwb <634700433@qq.com>
2020-06-10 14:31:56 +0800

    异常捕获

--- src/main/java/com/zwb/exception/SellException.java
+++ src/main/java/com/zwb/exception/SellException.java
@@ -1,12 +1,14 @@
 package com.zwb.exception;
 
 import com.zwb.enums.ResultEnum;
+import lombok.Getter;
 
 /**
  * @Desc:
  * @Author: zwb
  * @CreateTime: 2020/5/23 21:42
  **/
+@Getter
 public class SellException extends RuntimeException{
 
     private Integer code;
--- src/main/java/com/zwb/handler/SellExceptionHandler.java
+++ src/main/java/com/zwb/handler/SellExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.zwb.handler;
 
 import com.zwb.config.ProjectUrlConfig;
+import com.zwb.exception.SellException;
 import com.zwb.exception.SellerAuthorizeException;
+import com.zwb.utils.ResultVOUtil;
+import com.zwb.vo.ResultVO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -32,5 +35,12 @@ public class SellExceptionHandler {
                 .concat("?returnUrl=")
                 .concat(projectUrlConfig.getSell())
                 .concat("/sell/seller/login"));
+    }
+
+    @ExceptionHandler(value = SellException.class)
+    @ResponseBody
+//    @ResponseStatus(HttpStatus.FORBIDDEN)  //修改返回状态码为403
+    public ResultVO handlerSellException(SellException e) {
+        return ResultVOUtil.error(e.getCode(),e.getMessage());
     }
 }
